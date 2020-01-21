f9df249a1f51767e1d801dd12295651fbe27d039
eduuh <edwinkamaumuraya0@outlook.com>
2020-01-21 21:15:25 +0300

    created a componet person

--- react_complete_guide/src/App.js
+++ react_complete_guide/src/App.js
@@ -1,15 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
- 
-    return (
-     
- 
-    );
-    // return React.createElement('div', { className: 'App' }, React.createElement("h1", null, "Does this exist"));
+  state = {
+    person: [
+      { name: "max", age: 28 },
+      { name: "Edwin", age: 22 }
+    ]
+  };
+
+  switchNameHandler = (newname) => {
+    this.setState({
+      person: [
+        { name: newname, age: 29 },
+        { name: "Edwin Kamau", age: 22 }
+      ]
+    });
   }
 
+  nameChangedHandler = (event)=>{
+    this.setState({
+      person:[
+          {name:event.target.value ,age: 28},
+          {name:'Edwin',age: 22}
+        
+      ]
+    })
+  }
+  render() {
 
+    const style = {
+      backgroudColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'poiter'
+    };
+
+    return (
+      <div className="App">
+        <h1>Hi My Name is edwin</h1>
+
+        <Person name="Max" age="28" />
+        <Person name="Edwin" age="22" />
+        <Person name="Jackson">Hobies: racing</Person>
+
+        <button style={style} onClick={() => this.switchNameHandler('Maximilian!!!')}>Switch Name</button>
+        <Person
+          click={this.switchNameHandler.bind(this,'Max!')}
+          changed={this.nameChangedHandler}
+          name={this.state.person[0].name}
+          age={this.state.person[0].age}
+        >My hobbies: Riding</Person>
+        <Person
+          name={this.state.person[1].name}
+          age={this.state.person[1].age}
+        ></Person>
+      </div>
+    );
+  }
+}
 export default App;
